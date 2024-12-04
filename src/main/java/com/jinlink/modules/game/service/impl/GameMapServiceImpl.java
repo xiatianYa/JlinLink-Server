81d8888f72a261b54fe2a1579b8bd58bcff5a896
xiatian <939648675@qq.com>
2024-12-04 23:06:10 +0800

    优化搜索地图配置label展示

--- src/main/java/com/jinlink/modules/game/service/impl/GameMapServiceImpl.java
+++ src/main/java/com/jinlink/modules/game/service/impl/GameMapServiceImpl.java
@@ -132,7 +132,7 @@ public class GameMapServiceImpl extends ServiceImpl<GameMapMapper, GameMap> impl
         List<GameMap> gameMapList = gameMapMapper.selectAll();
         return gameMapList.stream()
                 .map(item -> Options.<String>builder()
-                        .label(item.getMapLabel())
+                        .label(item.getMapLabel()+"("+item.getMapUrl()+")")
                         .value(String.valueOf(item.getId()))
                         .build())
                 .toList();
